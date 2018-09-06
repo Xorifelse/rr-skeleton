c16a2a5412e5c71708d1a99c8fe87116f4a06e4a
Xorifelse <johan.kruse@gmail.com>
2018-09-06 13:27:48 +0200

    Update store.js
    Fixed removed middleware

--- src/store.js
+++ src/store.js
@@ -5,6 +5,5 @@ import ReduxThunk from 'redux-thunk'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default createStore(reducer, composeEnhancers(
-  applyMiddleware(ReduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(ReduxThunk)
 ))
