51b969ab414db58aaa6cdc93226e7232933e1259
Xorifelse <johan.kruse@gmail.com>
2018-09-06 12:56:52 +0200

    Update store.js
    Fixed enhancer to work on mobile

--- src/store.js
+++ src/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import ReduxThunk from 'redux-thunk'
 
-export default createStore(reducer, compose(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default createStore(reducer, composeEnhancers(
   applyMiddleware(ReduxThunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
